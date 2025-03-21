e375f25c3ef38defd8c0464325c3b0914156fbb5
AugustineBlewu1 <blewudziaugustine.com>
2025-03-21 08:15:15 +0000

    feat: components and changes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,25 @@
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Text, View, Image, ScrollView } from "react-native";
 
 export default function Index() {
   return (
     <View
-      className="flex-1 items-center justify-center"
-    >
-      <Text className="text-dark-200 font-bold text-4xl">Welcome React Native</Text>
+      className="flex-1 bg-primary" 
 
-      
+    >
+      <Image source={images.bg} className="absolute w-full z-0" />
+
+      <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{paddingBottom: 100, minHeight: '100%'}}>
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+
+        <View className="flex-1 mt-5">
+          <SearchBar></SearchBar>
+        </View>
+      </ScrollView>
+     
     </View>
   );
 }
